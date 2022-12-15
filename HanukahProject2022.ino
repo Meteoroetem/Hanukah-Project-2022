8f45dace8cbd0d41631916007bdc18ba466958ff
Meteoroetem <114942884+Meteoroetem@users.noreply.github.com>
2022-12-15 21:41:46 +0200

    Finsished the song and lights

--- HanukahProject2022.ino
+++ HanukahProject2022.ino
@@ -6,15 +6,21 @@
 
 #define NUMPIXELS 16
 #define PIN 5
+# define my_sizeof(type) ((char *)(&type+1)-(char*)(&type))
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int potentVal;
 int delayval = 100; // timing delay in milliseconds
 
 int songStartTime = 0;
-int notes[] = {440,440,494,523,523,494};
-int lengths[] = {500,500,1000,500,500,1000};
-float songSpeed = 1.7f;
+int notes[] = {440,440,494,523,523,494,659,659,587,523,494,
+523,523,494,440,440,494,523,523,587,659,659,659,330,440,
+440,440,587,587,587,698,659,659,523,494,440,415,415,330,
+415,440,494,523,494,440,587,587,587,698,659,659,523,494,440,415,415,330,415,440,440,440};
+int lengths[] = {500,500,1000,500,500,1000,500,500,200,200,200,500,500,1000,500,500,1000
+,500,500,1000,500,500,500,500,500,500,1000,500,500,500,500,500,500,200,200,500,500,500,500,
+500,500,500,200,200,500,500,500,500,500,500,500,200,200,500,500,500,500,500,500,500,1000};
+float songSpeed = 2;
 
 int redColor = 200;
 int greenColor = 0;
@@ -54,7 +60,7 @@ void loop()
     if(button.isReleased()){
       songStartTime = millis();
       //tone(9,notes[0]);
-      for(int i = 0; i < 6; i++){
+      for(int i = 0; i < my_sizeof(notes)/my_sizeof(notes[0]); i++){
 
         tone(9,notes[i]);
 
@@ -62,7 +68,7 @@ void loop()
         for (int i = currentLed; i < currentLed+4; i++)
         {
           pixels.setPixelColor(i, pixels.Color(redColor, greenColor, blueColor));
-          pixels.setBrightness(100);
+          pixels.setBrightness(15);
           pixels.show();
           delay(50);
         }
@@ -76,7 +82,7 @@ void loop()
         
         delay((1/songSpeed)*lengths[i]);
         noTone(9);
-        delay(100);
+        delay(10);
       }
       
       /*for(int a = 0; a < 5; a++){
@@ -97,6 +103,7 @@ void loop()
 
         delay(delayval);        
       }
+      currentLed = 0;
     }
   #pragma region print_stuff
 	  Serial.print(digitalRead(4));
